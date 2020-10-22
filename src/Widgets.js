b23368e78e678c7f31f4fbef8875b761733b0f19
Ankush Chauhan <67872399+An-prog-hub@users.noreply.github.com>
2020-10-22 10:44:11 +0530

    Update Widgets.js

--- src/Widgets.js
+++ src/Widgets.js
@@ -3,6 +3,8 @@ import "./Widgets.css";
 function Widgets() {
   return (
     <div className="widgets">
+    // for the side portion of the page
+    // to get the iframe went ahead - > https://developers.facebook.com/docs/plugins/page-plugin/
       <iframe
         src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FTeslaMotorsCorp&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
         width="340"
